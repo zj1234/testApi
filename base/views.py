5d0b75e5440202560361ca33cc78bd983b2d53c0
Aldo <acruz@atentus.com>
2021-10-31 18:27:53 -0300

    primera respuesta

--- base/views.py
+++ base/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Token
+from .models import Token, Client
 from public.models import Auth
 from utils import error_api
+from hashlib import md5
 import json
+import ast
 # Create your views here.
 
 class AuthReportView(APIView):
@@ -16,14 +18,16 @@ class AuthReportView(APIView):
             else:
                 data = request.data
             user=str(data["user"])
-            passW=str(data["pass"])
-            _dataUserPass=Auth.objects.values("passText")
-            print(_dataUser)
-            _dataToken= Token.objects.values("cliente_usuario_id", "\
-                name").filter(token=token)
+            passW = str(md5(data["pass"].encode()).hexdigest())
+            _dataUserPass=Auth.objects.filter(passText=passW, user=user)
             
-            if _dataUser:
-                return Response(True)
+            if _dataUserPass:
+                #print("datauser",_dataUserPass, user)
+                _UserInfo=Client.objects.filter(name=user).values("perfil_id",\
+                    "name", "mail", "cell", "cars", "cliente_usuario_id" ).get()
+                _dataToken= Token.objects.values("token").filter(cliente_usuario_id=\
+                    _UserInfo["cliente_usuario_id"]).get()
+                return Response({"token":_dataToken, "user":_UserInfo})
             else:
                 return Response(error_api.ApiErrorCodesMessages.AuthToken())
         except Exception as e:
