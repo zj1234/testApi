23c82b66d9c8e8c393ca82cc940b2ec17f9adc52
Aldo <acruz@atentus.com>
2021-10-31 17:47:24 -0300

    base auth con user y pass

--- base/models.py
+++ base/models.py
@@ -2,17 +2,64 @@ from __future__ import unicode_literals
 
 from django.db import models
 from utils.date import datetimeExpired
-
+from django.db.models import JSONField
 
 class Token(models.Model):
 
     cliente_usuario_id = models.IntegerField(unique=True)
-    cliente_id = models.IntegerField(blank=False, default=1)
     token = models.CharField(("Key"), max_length=40, primary_key=True)
-    name = models.TextField()
     created = models.DateTimeField(("created"), auto_now_add=True)
     expired = models.DateTimeField(("expired"),default=datetimeExpired())
 
     class Meta:
         managed = False
         db_table = 'token'
+
+
+class Client(models.Model):
+
+    cliente_usuario_id = models.IntegerField(unique=True, primary_key=True)
+    perfil_id=models.IntegerField(blank=False)
+    name = models.TextField(blank=False)
+    mail = models.TextField()
+    cell=models.IntegerField()
+    cars =JSONField()
+
+    class Meta:
+        managed = False
+        db_table = 'client'
+    
+class Car(models.Model):
+
+    id_ingreso = models.IntegerField(unique=True, primary_key=True)
+    model = models.TextField(blank=False)
+    patente = models.TextField(blank=False)
+    id_reparacion=JSONField()
+    id_client =models.IntegerField(blank=False)
+
+    class Meta:
+        managed = False
+        db_table = 'car'
+
+class Repair(models.Model):
+
+    id = models.IntegerField(unique=True, primary_key=True)
+    id_car = models.IntegerField(blank=False)
+    description=models.TextField(blank=False)
+    entryDate=models.DateTimeField(blank=False)
+    finalDate=models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'repair'
+
+
+class Perfil(models.Model):
+
+    id = models.IntegerField(blank=False, primary_key=True)
+    name = models.TextField(blank=False)
+    description = models.TextField(blank=False)
+
+    class Meta:
+        managed = False
+        db_table = 'client'
--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Token
+from public.models import Auth
 from utils import error_api
 import json
 # Create your views here.
@@ -14,10 +15,13 @@ class AuthReportView(APIView):
                 data = json.loads(request.body)
             else:
                 data = request.data
-            token=data["token"]
-            _dataUser= Token.objects.values("cliente_id", \
-                "cliente_usuario_id", "name").filter(token=token)
+            user=str(data["user"])
+            passW=str(data["pass"])
+            _dataUserPass=Auth.objects.values("passText")
             print(_dataUser)
+            _dataToken= Token.objects.values("cliente_usuario_id", "\
+                name").filter(token=token)
+            
             if _dataUser:
                 return Response(True)
             else:
